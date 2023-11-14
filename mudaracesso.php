cfa002f4aa956a48f0b3f5a0ec0d3448d844c3c5
Hughertft <149502167+Hughertft@users.noreply.github.com>
2023-11-14 11:52:31 -0300

    Update mudaracesso.php

--- mudaracesso.php
+++ mudaracesso.php
@@ -6,6 +6,10 @@ session_start();
 include('conexao.php');
 include('validaradmin.php');
 
+$select = "SELECT nome, descricao, nivel.id, login.cpf FROM usuario INNER JOIN login ON usuario.cpf = login.cpf INNER JOIN nivel ON nivel.id = nivel";
+$queryselect = mysqli_query($conexao, $select);    
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,6 +21,33 @@ include('validaradmin.php');
 </head>
 <body>
     <center>
+        <form name="mudatipo" action="mudartipo.php" method="POST">
+            <table border="1px">
+                <tr>
+                    <td>Nome</td>
+                    <td>Tipo de Usuário</td>
+                    <td>Novo Tipo de Usuário</td>
+                    <td>Alterar</td>
+                </tr>
+                <?php
+                While ($linha = mysqli_fetch_row($queryselect)) { ?>
+                <tr>
+                    <td><?php echo $linha[0]?></td>
+                    <td><?php echo $linha[1]?></td>
+                    <td>
+                        <select name="nivel">
+                            <option value="1">administrador</option value="3">Usuário</option</select>
+                </td>
+                <td>
+                    <input type="submit" name="alterar" value="Alterar">
+                    <input type="hidden" name="cpf" value="<?php echo $linha[3]?>">
+                </td>
+                </tr>
+                <?php 
+                } 
+                ?>
+                </table>
+                </form>
 
     </center>
 </body>
